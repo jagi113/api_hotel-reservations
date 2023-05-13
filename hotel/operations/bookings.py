1df6f0cf9a613f4ce747c843c49acbe4a6a5d932
jagi113 <jaroslavgirovsky@gmail.com>
2023-05-13 13:18:55 +0200

    bookings_abstraction

--- hotel/operations/bookings.py
+++ hotel/operations/bookings.py
@@ -1,10 +1,8 @@
 from datetime import date
-from typing import Optional
 
 from pydantic import BaseModel
 
-from hotel.db.engine import DBSession
-from hotel.db.models import DBBooking, DBRoom, to_dict
+from hotel.operations.interface import DataInterface, DataObject
 
 
 class InvalidDateError(Exception):
@@ -18,37 +16,31 @@ class BookingCreateData(BaseModel):
     to_date: date
 
 
-def read_all_bookings():
-    session = DBSession()
-    bookings = session.query(DBBooking).all()
-    return [to_dict(booking) for booking in bookings]
+def read_all_bookings(booking_interface: DataInterface) -> list[DataObject]:
+    return booking_interface.read_all()
 
 
-def read_booking(booking_id: int):
-    session = DBSession()
-    booking = session.query(DBBooking).get(booking_id)
-    return to_dict(booking)
+def read_booking(booking_id: int, booking_interface: DataInterface) -> DataObject:
+    return booking_interface.read_by_id(booking_id)
 
 
-def create_booking(data: BookingCreateData):
-    session = DBSession()
+def create_booking(
+    data: BookingCreateData,
+    booking_interface: DataInterface,
+    room_interface: DataInterface,
+) -> DataObject:
     # retrieve the room info
-    room = session.query(DBRoom).get(data.room_id)
+    room = room_interface.read_by_id(data.room_id)
     days = (data.to_date - data.from_date).days
     if days <= 0:
         raise InvalidDateError("Invalid dates.")
     booking_dict = data.dict()
-    booking_dict["price"] = days * room.price
-
-    booking = DBBooking(**booking_dict)
-    session.add(booking)
-    session.commit()
-    return to_dict(booking)
+    booking_dict["price"] = days * room["price"]
+    return booking_interface.create(booking_dict)
 
 
-def delete_booking(booking_id: int):
-    session = DBSession()
-    booking = session.query(DBBooking).get(booking_id)
-    session.delete(booking)
-    session.commit()
-    return to_dict(booking)
+def delete_booking(
+    booking_id: int,
+    booking_interface: DataInterface,
+) -> DataObject:
+    return booking_interface.delete(booking_id)
--- hotel/routers/bookings.py
+++ hotel/routers/bookings.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 
+from hotel.db.interface import DBInterface
+from hotel.db.models import DBBooking, DBRoom
 from hotel.operations.bookings import (
     BookingCreateData,
     create_booking,
@@ -13,19 +15,24 @@ router = APIRouter()
 
 @router.get("/bookings")
 def api_read_all_bookings():
-    return read_all_bookings()
+    booking_interface = DBInterface(DBBooking)
+    return read_all_bookings(booking_interface)
 
 
-@router.get("/booking/{booking_id}")
+@router.get("/bookings/{booking_id}")
 def api_read_booking(booking_id: int):
-    return read_booking(booking_id)
+    booking_interface = DBInterface(DBBooking)
+    return read_booking(booking_id, booking_interface)
 
 
-@router.post("/booking")
+@router.post("/bookings")
 def api_create_booking(booking: BookingCreateData):
-    return create_booking(booking)
+    booking_interface = DBInterface(DBBooking)
+    room_interface = DBInterface(DBRoom)
+    return create_booking(booking, booking_interface, room_interface)
 
 
-@router.delete("/booking/{booking_id}")
+@router.delete("/bookings/{booking_id}")
 def api_delete_booking(booking_id: int):
-    return delete_booking(booking_id)
+    booking_interface = DBInterface(DBBooking)
+    return delete_booking(booking_id, booking_interface)
